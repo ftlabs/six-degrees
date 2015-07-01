24eaeac90b25d524e44e8c9244fd2b3aac6787be
Ada Rose Edwards <ada.edwards@ft.com>
2015-07-01 18:33:42 +0100

    add service worker

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
-(() => {
-	console.log('Hi');
-})();
+/* jshint browser:true */
+
+// Start service worker
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js', { scope: '/' })
+		.then(function(reg) {
+			console.log('sw registered', reg);
+		}).catch(function(error) {
+			console.log('sw registration failed with ' + error);
+		});
+}
