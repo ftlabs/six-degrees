44ef6c5f869c63eec92a1b7e73948431e7b3c423
Andrew Betts <andrew.betts@ft.com>
2015-07-07 15:09:37 +0100

    WIP on Erdos

--- app/scripts/erdos-demo.js
+++ app/scripts/erdos-demo.js
@@ -1,10 +1,24 @@
 'use strict';
 
-require('./lib/data.js')
-	.then(function(data) {
-		console.log(data);
-		require('./lib/d3.js')(data, {
-			place: '.o-techdocs-content',
-			width: document.querySelector('.o-techdocs-content').clientWidth
+var dom = {
+	datalist: document.querySelector('#data-people'),
+	form: document.querySelector('#erdos-form'),
+}
+
+fetch('http://ftlabs-sapi-capi-slurp.herokuapp.com/metadatums/by_type/people')
+	.then(response => response.text())
+	.then(string => JSON.parse(string))
+	.then(json => json.metadatums_by_type.people)
+	.then(function (people) {
+		.innerHTML = people.map(p => '<option value="' + p.slice(7) + '">').join('');
+		return new Promise(function (resolve) {
+			dom.form.addEventListener('submit', function (e) {
+				e.preventDefault();
+				if (e.target.elements[0].value) {
+					selectedPerson = `people:${e.target.elements[0].value}`;
+					// TODO
+				}
+			});
 		});
-	});
+	})
+;
