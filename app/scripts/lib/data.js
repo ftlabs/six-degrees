082ce98982fc9d0e7a3c8e92a4dde4fca71ecf4f
Ada Rose Edwards <ada.edwards@ft.com>
2015-07-07 12:20:10 -0700

    Add affect of weights

--- app/scripts/lib/data.js
+++ app/scripts/lib/data.js
@@ -1,4 +1,4 @@
-/* global fetch */
+/* global fetch, atob */
 
 'use strict';
 
@@ -6,46 +6,69 @@
 // pick one
 // fetch their coocurring people
 // then fetch theirs
-
+const pako = require('pako');
 const ui = require('./ui');
-const MAX_POPULATION = 10;
-let selectedPerson;
-let populus = [];
+
+const populus = [];
 
 class Person {
-	constructor({
-		name
-	}) {
-		if (typeof weight === undefined) {
-			throw Error('Can\'t construct person without weight');
+	constructor(personData) {
+		this.name = personData.name,
+		this.label = personData.name,
+		this.numberOfOccurences = personData.numberOfOccurences,
+		this.id = personData.id,
+		this.isAmbassador = personData.isAmbassador,
+		this.island = personData.island;
+		this.islandIndex = personData.islandIndex;
+
+		//unpack island
+		if (typeof this.island.connections.unpacked === 'undefined') {
+			this.island.connections.unpacked = JSON.parse(pako.inflate(atob(this.island.connections.json_zlib_base64), {to: 'string'}));
 		}
-		if (typeof weight === undefined) {
-			throw Error('Can\'t construct person without weight');
-		}
-		this.name = name.slice(7);
-		this.label = name.slice(7);
-		this.id = name;
-		this.connections = new Set();
-		this.connectionWeights = new Map();
 	}
 
-	connect(targetPerson) {
-		this.connections.add(targetPerson);
+	getConnections(maxDepth=1, depth=1) {
 
-		// Add self to raget if not already added
-		if (!targetPerson.isConnectedTo(this)) {
-			targetPerson.connect(this);
+		const collectedPeople = new Set();
+
+		this.connectionWeights = new Map();
+
+		// create people for each connection.
+		this.connections = new Set(
+			this.island.connections.unpacked[this.islandIndex]
+				.map((numberOfConnections, i) => {
+					if (numberOfConnections === 0) return false;
+
+					const connectedPerson = getOrCreatePerson(this.island.islanders[i]);
+					this.connectionWeights.set(connectedPerson, numberOfConnections);
+					return connectedPerson;
+				})
+				.filter(p => p !== false)
+		);
+
+		this.connections.forEach(c => collectedPeople.add(c));
+
+		if (depth < maxDepth) {
+			this.connections.forEach(person => person
+				.getConnections(maxDepth, depth + 1)
+				.forEach(c => collectedPeople.add(c))
+			);
 		}
+
+		return collectedPeople;
 	}
 
 	isConnectedTo(person) {
+		if (typeof this.connections === 'undefined') {
+			this.getConnections(1);
+		}
 		return this.connections.has(person);
 	}
 }
 
 function getOrCreatePerson(options) {
 	for (let p of populus) {
-		if (p.id === options.name) {
+		if (p.id === options.id) {
 			return p;
 		}
 	}
@@ -54,98 +77,105 @@ function getOrCreatePerson(options) {
 	return np;
 }
 
-function fetchPerson(person, maxDepth=1, currentDepth=0) {
-	return fetch(`https://ftlabs-sapi-capi-slurp.herokuapp.com/cooccurrences_as_counts/${person.id}/by_type/people`)
+function fetchJSON(url) {
+	return fetch(url)
 		.then(response => response.text())
-		.then(string => JSON.parse(string))
-		.then(json => json.cooccurrences_as_counts.people)
-		.then(function(list) {
-			var maxCount = list[0][1];
-
-			return list.map(function(item) {
-				return new Person({
-					name: `people:${item[0]}`,
-					count: item[1],
-					weight: item[1] / maxCount
-				});
-			});
-		})
-		.then(function (list) {
-
-			// add new people to the people list and sort out
-			return list.map(p => {
-				let np = getOrCreatePerson(p);
-				np.connect(person);
-				if (populus.length < MAX_POPULATION) return fetchPerson(np, maxDepth, currentDepth+1);
-				return null;
-			});
-		})
-		.then(promiseList => Promise.all(promiseList))
-		.then(function () {
-			return populus;
-		});
+		.then(string => JSON.parse(string));
 }
 
-module.exports = fetch('https://ftlabs-sapi-capi-slurp.herokuapp.com/metadatums/by_type/people')
-	.then(response => response.text())
-	.then(string => JSON.parse(string))
-	.then(json => json.metadatums_by_type.people)
+module.exports = Promise.all([
+		fetchJSON('https://ftlabs-sapi-capi-slurp.herokuapp.com/metadatums/by_type/people'),
+		fetchJSON('http://ftlabs-sapi-capi-slurp.herokuapp.com/erdos_islands_of/people/with_connectivity'),
+	])
+	.then(([peopleJson, islandsJSON]) => [peopleJson.metadatums_by_type.people, islandsJSON])
+	.then(function ([people, islandsJSON]) {
+
+		const unifiedData = {};
+
+		people.map(function (p) {
+			unifiedData[p] = {
+				id: p,
+				name: p.slice(7)
+			};
+			return unifiedData;
+		});
+
+		islandsJSON.islands.forEach(function (island) {
+
+			island.maxConnections = island.ambassador[1];
+
+			island.islanders = island.islanders.map(function (islander, index) {
+				const id = islander[0];
+				unifiedData[id].numberOfOccurences = islander[1];
+				unifiedData[id].isAmbassador = (id === island.ambassador[0]);
+				unifiedData[id].island = island;
+				unifiedData[id].islandIndex = index;
+
+				// update the source data to have objects
+				// rather than arrays.
+				return unifiedData[id];
+			});
+		});
+
+		return unifiedData;
+	})
 	.then(function (people) {
 
-		return ui.modal('.o-techdocs-main', `
+		return [people, ui.modal('.o-techdocs-main', `
 			<form>
 				<label for="people">Select a starting person
 				<input list="people" name="people">
 				<datalist id="people">
-					${people.map(p => '<option value="' + p.slice(7) + '">').join('')}
+					${Object.keys(people).map(p => '<option value="' + people[p].name + '">').join('')}
 				</datalist></label>
 				<submit>
-			</form>`);
+			</form>`)];
 	})
-	.then(function (modal) {
+	.then(function ([people, modal]) {
+		modal.remove();
+		return people[`people:Yoko Ono`];
 		return new Promise(function (resolve) {
 			modal.el.querySelector('form').addEventListener('submit', function (e) {
 				e.preventDefault();
 				if (e.target.elements[0].value) {
 					modal.remove();
-					selectedPerson = `people:${e.target.elements[0].value}`;
-					resolve(selectedPerson);
+					resolve(people[`people:${e.target.elements[0].value}`]);
 				}
 			});
 		});
 	})
-	.then(name => getOrCreatePerson({name}))
-	.then(fetchPerson, 2)
-	.then(function (peopleArray) {
+	.then(personData => getOrCreatePerson(personData).getConnections(2))
+	.then(connectedPeeps => Array.from(connectedPeeps))
+	.then(connectedPeeps => {
 
 		const labelAnchors = [];
 		const labelAnchorLinks = [];
 		const links = [];
 
-		if (peopleArray.length === 1) {
-			throw Error(`Not enough people connected to ${selectedPerson}.`);
+		if (connectedPeeps.length === 1) {
+			throw Error(`Not enough people connected`);
 		}
 
-		const nPeople = peopleArray.length;
+		const nPeople = connectedPeeps.length;
 
 		// Create nodes
 		for(let i = 0; i < nPeople; i++) {
 			labelAnchors.push({
-				node: peopleArray[i]
+				node: connectedPeeps[i]
 			});
 			labelAnchors.push({
-				node: peopleArray[i]
+				node: connectedPeeps[i]
 			});
 		}
 
 		// Link up nodes
-		for(var i = 0; i < peopleArray.length; i++) {
+		for(var i = 0; i < nPeople; i++) {
 			for(var j = 0; j < i; j++) {
-				if(peopleArray[j].isConnectedTo(peopleArray[i])) {
+				if(connectedPeeps[j].isConnectedTo(connectedPeeps[i])) {
 					links.push({
 						source : i,
 						target : j,
-						weight : 0.01
+						weight : connectedPeeps[j].connectionWeights.get(connectedPeeps[i])/connectedPeeps[j].island.maxConnections
 					});
 				}
 			}
@@ -157,7 +187,7 @@ module.exports = fetch('https://ftlabs-sapi-capi-slurp.herokuapp.com/metadatums/
 		}
 
 		return {
-			nodes: peopleArray,
+			nodes: connectedPeeps,
 			labelAnchors,
 			labelAnchorLinks,
 			links
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,27 +13,9 @@ function addScript(url) {
 	});
 }
 
-// Start service worker
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw.js')
-		.then(function(reg) {
-			console.log('sw registered', reg);
-		}).catch(function(error) {
-			console.log('sw registration failed with ' + error);
-		});
-
-	// if the service worker is running...
-	// We don't want to make 1000s of api requests without
-	// some caching for next time.
-	if (navigator.serviceWorker.controller) {
-		console.log('Offlining Availble');
-		document.body.classList.add('sw-ready');
-
-		Promise.all([
-			addScript('https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'),
-			addScript('https://polyfill.webservices.ft.com/v1/polyfill.min.js?features=fetch,default,Array.from')
-		]).then(function () {
-			addScript('./scripts/demo.js');
-		});
-	}
-}
+Promise.all([
+	addScript('https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'),
+	addScript('https://polyfill.webservices.ft.com/v1/polyfill.min.js?features=fetch,default')
+]).then(function () {
+	addScript('./scripts/demo.js');
+});
