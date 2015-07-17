39253a00a103e3323aaa6052592e07f900247850
Ada Rose Edwards <ada.edwards@ft.com>
2015-07-17 10:12:50 -0700

    relayout to be on the side rather as a footer

--- app/scripts/demo.js
+++ app/scripts/demo.js
@@ -5,7 +5,7 @@ require('babel/polyfill');
 const data = require('./lib/data.js');
 require('./lib/d3.js')({
 	generator: data.generator,
-	place: '.o-techdocs-content',
-	width: document.querySelector('.o-techdocs-content').clientWidth,
+	place: '.graph-area',
+	width: document.querySelector('.graph-area').clientWidth,
 	height: document.querySelector('body').clientHeight
 });
